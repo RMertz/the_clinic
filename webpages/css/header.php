f92072d6c2b26855573ac3f924d4b77cfeb3c954
Christopher <christomill@yahoo.com>
2019-03-29 08:42:10 -0600

    Fixed relative pathing issues of header.php

--- webpages/css/header.php
+++ webpages/css/header.php
@@ -7,7 +7,15 @@
         </div>
         <div class= "column right">
             <div id="headerLogo">
-                <img src="../images/HeaderImageOutline.png" alt="HeaderImage">
+                <img src=<?php
+                if(getcwd() == "/Users/chrismiller/Documents/CSCI/ESOF423/the_clinic/webpages/bipolar/bipolarM"||getcwd() == "/Users/chrismiller/Documents/CSCI/ESOF423/the_clinic/webpages/bipolar/bipolarD"){
+                    echo "../../images/HeaderImageOutline.png";
+                }elseif (getcwd() == "/Users/chrismiller/Documents/CSCI/ESOF423/the_clinic/webpages"){
+                    echo "images/HeaderImageOutline.png";
+                }else{
+                    echo "../images/HeaderImageOutline.png";
+                }
+                ?> alt="HeaderImage">
             </div>
         </div>
     </div>
