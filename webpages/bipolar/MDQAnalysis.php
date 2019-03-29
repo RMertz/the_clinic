701a1556990f9cdf001620d3ff9e6a3daef370de
Christopher <christomill@yahoo.com>
2019-03-28 23:46:44 -0600

    Fixed link to patient home in file MDQAnalysis.php

--- webpages/bipolar/MDQAnalysis.php
+++ webpages/bipolar/MDQAnalysis.php
@@ -43,7 +43,7 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
 <div class="navBar">
     <a class="<?= ($activePage == 'welcome') ? 'active':''; ?>" href="../welcome.php">Home</a>
     <a class="<?= ($activePage == 'patientList') ? 'active':''; ?>" href="../patientList.php">Your Patients</a>
-    <a class="<?= ($activePage == 'patientHome') ? 'active':''; ?>" href=<?php echo "patientHome.php?id=".$_GET['id'];?>>Patient Home</a>
+    <a class="<?= ($activePage == 'patientHome') ? 'active':''; ?>" href=<?php echo "../patientHome.php?id=".$_GET['id'];?>>Patient Home</a>
     <a class="<?= ($activePage == 'depHome') ? 'active':''; ?>"href= <?php echo "../dep/depHome.php?id=".$_GET['id'];?>>Depression Treatment</a>
     <a class="<?= ($activePage == 'depDiag') ? 'active':''; ?>"href= <?php echo "../dep/depDiag.php?id=".$_GET['id'];?>>Depression PHQ</a>
     <a class="<?= ($activePage == 'bipolarHome') ? 'active':''; ?>" href=<?php echo "bipolarHome.php?id=".$_GET['id'];?>>Bipolar Treatment</a>
