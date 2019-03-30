c22f9d62a11f1f349cf45b7729102703b69b275a
RMertz <antonmertz@gmail.com>
2019-03-30 13:06:31 -0600

    Testing database functionality

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -1,23 +1,17 @@
 <?php
 declare(strict_types=1);
 
-use Config;
+use master;
 require __DIR__ . "/../webpages/Email.php";
 use PHPUnit\Framework\TestCase;
 
 final class EmailTest extends TestCase
 {
-    public function testCanBeCreatedFromValidEmailAddress(): void
-    {
-        $this->assertInstanceOf(
-            Email::class,
-            Email::fromString('user@example.com')
-        );
-    }
 
     public function testCannotBeCreatedFromInvalidEmailAddress(): void
     {
-        $this->expectException(InvalidArgumentException::class);
+	start();    
+	$this->expectException(InvalidArgumentException::class);
 
         Email::fromString('invalid');
     }
---
+++ tests/master.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use Config;
+
+final class master
+{
+    public function start(): void
+    {
+	session_start();
+    }
+}
