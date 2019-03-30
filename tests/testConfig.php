a20b7632c40a5cb9199d79ed7131b83a55c76199
RMertz <antonmertz@gmail.com>
2019-03-30 15:09:07 -0600

    Update database testing configuration

--- tests/testConfig.php
+++ tests/testConfig.php
@@ -1,6 +1,7 @@
 <?php
 try {
-    $db = new PDO('mysql:host=localhost:127.0.0.1;dbname=group1', 'Chris1', 'Test123',array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $dsn = 'mysql:host=localhost;dbname=test;charset=utf8';
+    $db = new PDO($dsn, 'travis', '');
 }catch(PDOException $ex){
     echo "<script>console.log('Failed to open database')</script>";
 }
