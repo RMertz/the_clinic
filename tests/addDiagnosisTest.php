e55adec361b3a8adbc99caca4984035ff10801cc
RMertz <antonmertz@gmail.com>
2019-04-03 12:55:10 -0600

    update test

--- tests/addDiagnosisTest.php
+++ tests/addDiagnosisTest.php
@@ -8,7 +8,7 @@ final class addDiagnosisTest extends TestCase
     public function testsInstantiation(): void
     {
 	    $masterTest = masterTest();
-	    $db = masterTest.getConnection();
+	    $db = masterTest->getConnection();
 	    session_start();
 	    $newaddDiagnosis = new addDiagnosis();
 	    $this->assertEquals("AddedDiagnosis",$newaddDiagnosis->addDiagnosisToPatient('123','diagnosis'));
