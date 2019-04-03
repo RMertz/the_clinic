aa810617d921cbb10497706fb0bd7a45e5959ab4
RMertz <antonmertz@gmail.com>
2019-04-03 10:50:21 -0600

    update tests

--- tests/addDiagnosisTest.php
+++ tests/addDiagnosisTest.php
@@ -1,13 +1,12 @@
-<?php include("master.php"); ?>
+<?php include("masterTest.php"); ?>
 <?php
+session_start();
 require __DIR__ . "/../webpages/php/addDiagnosis.php";
 use PHPUnit\Framework\TestCase;
 final class addDiagnosisTest extends TestCase
 {
     public function testsInstantiation(): void
     {
-	    $master = new master();
-	    $master->start(); 
 	    $newaddDiagnosis = new addDiagnosis();
 	    $this->assertEquals("AddedDiagnosis",$newaddDiagnosis->addDiagnosisToPatient('123','diagnosis'));
 	     
