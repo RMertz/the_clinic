e1588ba70ae76d8fbc8b0bf999b480324c78ebe5
RMertz <antonmertz@gmail.com>
2019-04-08 11:27:10 -0600

    Update testConfig.php

--- tests/testConfig.php
+++ tests/testConfig.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $dsn = 'mysql:dbname=test;host = 127i.0.0.1;charset=utf8';
+    $dsn = 'mysql:dbname=test;host = 127.0.0.1;charset=utf8';
     $db = new PDO($dsn, 'travis', '');
 }catch(PDOException $ex){
     echo "<script>console.log('Failed to open database')</script>";
