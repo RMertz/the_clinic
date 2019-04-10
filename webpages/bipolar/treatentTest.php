1034bf41eb71260b191ebad3ecab2f1cbd524e45
Christopher <christomill@yahoo.com>
2019-04-10 10:22:32 -0600

    try initial mustache.js files

---
+++ webpages/bipolar/treatentTest.php
@@ -0,0 +1,11 @@
+<?php
+include_once ("../js/treatment/treatmentGen.js");?>
+<!DOCTYPE HTML>
+<html>
+<body onload="loadUser()">
+<div id="target">Loading...</div>
+<script id="template" type="x-tmpl-mustache">
+Hello {{ name }}!
+</script>
+</body>
+</html>
