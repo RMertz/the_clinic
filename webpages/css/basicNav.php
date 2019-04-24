112591c325f32c1f89b670211e8f7bf1a7307984
Christopher <christomill@yahoo.com>
2019-04-24 10:45:46 -0600

    fixing home pages nav

--- webpages/css/basicNav.php
+++ webpages/css/basicNav.php
@@ -3,5 +3,5 @@
 <div class="navBar">
     <a class="<?= ($activePage == 'index') ? 'active':''; ?>" href=<?php echo "/group1/the_clinic/webpages/index.php"?>>Home</a>
     <a class="<?= ($activePage == 'help') ? 'active':''; ?>"href=<?php echo "/group1/the_clinic/webpages/help.php"?>>Help</a>
-    <a id="logoutButton" href = "/group1/the_clinic/webpages/php/Login.php">Sign In</a>
+    <a id="logoutButton" href = "/group1/the_clinic/webpages/Login.php">Sign In</a>
 </div>
