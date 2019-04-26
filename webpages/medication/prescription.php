08e5057dce3b3dd6f22efd89cef132719d7ec5ed
Christopher <christomill@yahoo.com>
2019-04-26 15:45:27 -0600

    testing joshes fix

--- webpages/medication/prescription.php
+++ webpages/medication/prescription.php
@@ -5,16 +5,25 @@ $medications = $db->prepare("SELECT * FROM MedicationInformation WHERE Medicatio
 $medications->bindParam(":medid", $_GET['medid']);
 $medications->execute();
 $row = $medications->fetch();
+
+$conflicts = $db->prepare("SELECT * FROM `MedicationInformation`
+        LEFT JOIN `Conflicting Medication` ON (`Conflicting Medication`.`MedicationID1`= `MedicationInformation`.`MedicationID`
+		AND `Conflicting Medication`.`MedicationID2` = :medid)
+		OR (`Conflicting Medication`.`MedicationID2` = :medid
+		AND `Conflicting Medication`.`MedicationID1` = :medid)");
+$conflicts->bindParam(":medid", $medid);
+$medications->execute();
+
 $error = " ";
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $update = new medicationControl();
-	$conflict = $update->checkConflict($_GET['id'],$_GET['medid']);
-	if ($conflict>0){
-		$error = "Error: Medication conflicts with current prescription";
+    $conflict = $update->checkConflict($_GET['id'],$_GET['medid']);
+    if ($conflict>0){
+        $error = "Error: Medication conflicts with current prescription";
     }
     else{
-		$error = $update->setDose($_GET['id'],$_GET['medid'],$_POST['dose']);
-	}
+        $error = $update->setDose($_GET['id'],$_GET['medid'],$_POST['dose']);
+    }
 };
 ?>
 <html xmlns="http://www.w3.org/1999/html">
@@ -32,26 +41,27 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <?php include('../css/header.php');
 include "../css/selectedPatientNav.php";?>
 
-    <div class="row">
-        <div class="column2" >
-            <h2>Medication Info</h2>
-            <p>Name: <?php echo $row['Name']?></p>
-            <p> Minimum Dose: <?php echo $row['MinimumDosage']?></p>
-            <p>  Maximum Dose: <?php echo $row['MaximumDosage']?></p>
+<div class="row">
+    <div class="column2" >
+        <h2>Medication Info</h2>
+        <p>Name: <?php echo $row['Name']?></p>
+        <p> Minimum Dose: <?php echo $row['MinimumDosage']?></p>
+        <p>  Maximum Dose: <?php echo $row['MaximumDosage']?></p>
+        <p>Medication That Conflicts for This Medication: <?php echo $conflicts['Name']?></p>
+    </div>
+    <div class="column2"
+    <div class= "loginBox">
+        <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Prescribe This Medication</b></div>
+        <div style = "padding:30px; background-color: #dfdce3; ">
+            <form action = "" method = "post">
+                <h3><?php echo $row['Name']?></h3>
+                <label>Dose :</label><input type = "text" name = "dose" class = "box" required/><br /><br />
+                <input type = "submit" value = " Submit "/><br />
+            </form>
+            <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
         </div>
-        <div class="column2"
-            <div class= "loginBox">
-                <div style = "background-color:#333333; color:#FFFFFF; padding:3px;"><b>Prescribe This Medication</b></div>
-                    <div style = "padding:30px; background-color: #dfdce3; ">
-                        <form action = "" method = "post">
-                            <h3><?php echo $row['Name']?></h3>
-                            <label>Dose :</label><input type = "text" name = "dose" class = "box" required/><br /><br />
-                            <input type = "submit" value = " Submit "/><br />
-                        </form>
-                        <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-                    </div>
-                </div>
-            </div>
-        </div>
+    </div>
+</div>
+</div>
 </body>
 </html>
